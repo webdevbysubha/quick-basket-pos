3bd2b383d98ec289780ca6a24d1a2fe33f1ee7b2
Subha Mondal <145588275+webdevbysubha@users.noreply.github.com>
2026-03-16 00:23:18 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,7 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
